18dd7af943a1fafdd599541f6c79e18940980ae4
Francis Bonacic <francisbonacic@icloud.com>
2021-02-09 18:17:46 -0800

    I am getting some errors / not able to move forward beyond task 1

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,22 @@
 // implement your posts router here
+
+const Posts = require("./posts-model")
+const express = require("express")
+const router = express.Router();
+
+//ENDPOINTS
+
+router.get('/api/posts', (req, res) => {
+    Posts.find()
+    .then( posts => {
+        res.status(200).json(posts)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json(error)
+    })
+})
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // require your server and launch it here
+
+const server = require('./api/server');
+
+server.listen(4000, () => {
+    console.log('server is running at http://localhost/4000')
+})
+
+
+
+
+//initial GET
+server.get('/', (req, res) => {
+    res.send(`<h1> server is running<h1/>`)
+})
